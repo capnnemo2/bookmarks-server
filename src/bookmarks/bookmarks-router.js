c2e5ac128b0f0bfb53b315a6a5fd1814a4871363
Ben Hernandez <benjammin234@gmail.com>
2020-02-20 17:20:29 -0800

    Activates GET bookmarks/:id endpoint

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -12,13 +12,55 @@ bookmarksRouter
     res.json(bookmarks);
   })
   .post(bodyParser, (req, res) => {
-    // here
+    const { title, url, description, rating } = req.body;
+    if (!title) {
+      logger.error("Title is required");
+      return res.status(400).send("Invalid data");
+    }
+
+    if (!url) {
+      logger.error("Url is required");
+      return res.status(400).send("Invalid data");
+    }
+
+    if (!description) {
+      logger.error("Description is required");
+      return res.status(400).send("Invalid data");
+    }
+
+    if (!rating) {
+      logger.error("Rating is required");
+      return res.status(400).send("Invalid data");
+    }
+
+    const id = uuid();
+    const bookmark = {
+      id,
+      title,
+      url,
+      description,
+      rating
+    };
+
+    bookmarks.push(bookmark);
+
+    logger.info(`Bookmark with id ${id} created`);
+
+    res.status(201).location(`http://localhost:8000/bookmark/${id}`);
   });
 
 bookmarksRouter
   .route("/bookmarks/:id")
   .get((req, res) => {
-    // here
+    const { id } = req.params;
+    const bookmark = bookmarks.find(b => b.id == id);
+
+    if (!bookmark) {
+      logger.error(`Bookmark with id ${id} not found`);
+      return res.status(404).send("Bookmark Not Found");
+    }
+
+    res.json(bookmark);
   })
   .delete((req, res) => {
     // here
