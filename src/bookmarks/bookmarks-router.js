b055b6e7c3440b098a5c5acbd1b4aa08f6120fbf
Ben Hernandez <benjammin234@gmail.com>
2020-02-20 17:02:22 -0800

    Creates logger module and bookmarks-router stubs

---
+++ src/bookmarks/bookmarks-router.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const uuid = require("uuid/v4");
+const logger = require("../logger");
+const { bookmarks } = require("../store");
+
+const bookmarksRouter = express.Router();
+const bodyParser = express.json();
+
+bookmarksRouter
+  .route("/bookmarks")
+  .get((req, res) => {
+    // here
+  })
+  .post(bodyParser, (req, res) => {
+    // here
+  });
+
+bookmarksRouter
+  .route("/bookmarks/:id")
+  .get((req, res) => {
+    // here
+  })
+  .delete((req, res) => {
+    // here
+  });
