c589653a43157e496db042d768871fb639cc25bb
Ben Hernandez <benjammin234@gmail.com>
2020-02-27 07:52:58 -0800

    Creates bookmarks fixtures

---
+++ test/bookmarks.fixtures.js
@@ -0,0 +1,29 @@
+function makeBookmarksArray() {
+  return [
+    {
+      id: 1,
+      title: "Bing",
+      url: "https://www.bing.com",
+      rating: 2
+    },
+    {
+      id: 2,
+      title: "Yahoo",
+      url: "https://www.yahoo.com",
+      rating: 1
+    },
+    {
+      id: 3,
+      title: "Ask Jeeves",
+      url: "https://www.askjeeves.com"
+    },
+    {
+      id: 4,
+      title: "BBC",
+      url: "https://www.bbc.com",
+      rating: 5
+    }
+  ];
+}
+
+module.exports = { makeBookmarksArray };
